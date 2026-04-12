66c818cc5ef923e0109119b38f87c844f035ce0f
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-04-12 09:24:22 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -5,7 +5,7 @@ int main() {
     
     for (int i=0; i<5; i++) {
         for (int j=0; ; j++) {
-        char c = names[names*i][j];
+        char c = *(*(names+i)+j);
         if (c=='\0') {
             break;
         }
